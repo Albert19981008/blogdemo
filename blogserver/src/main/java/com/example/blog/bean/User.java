2c3a514c8caf2b65af588b0974f2f8c8efdab886
lixinyang <741162208@qq.com>
2020-01-09 02:15:06 +0800

    配置好同时访问jsp和静态页面的环境

--- blogserver/src/main/java/com/example/blog/bean/User.java
+++ blogserver/src/main/java/com/example/blog/bean/User.java
@@ -1,12 +1,12 @@
 package com.example.blog.bean;
 
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 
-public class User { //implements UserDetails {
+public class User implements UserDetails {
 
     private Integer id;
     private String username;
@@ -23,40 +23,40 @@ public class User { //implements UserDetails {
         this.role = role;
     }
 
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     public Integer getId() {
         return id;
--- blogserver/src/main/java/com/example/blog/service/impl/UserServiceImpl.java
+++ blogserver/src/main/java/com/example/blog/service/impl/UserServiceImpl.java
@@ -4,8 +4,8 @@ import com.example.blog.bean.User;
 import com.example.blog.dao.UserDao;
 import com.example.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
@@ -15,10 +15,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
-//    @Override
-//    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-//        return userDao.loadUserByUsername(s);
-//    }
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userDao.loadUserByUsername(s);
+    }
 
     @Override
     public int addUser(String username, String password) {
