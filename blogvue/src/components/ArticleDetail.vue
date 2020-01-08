502999656bdfe3a18de1015bc24502bcd0bee599
lixinyang <741162208@qq.com>
2020-01-08 22:40:42 +0800

    完善文章详情

--- blogvue/src/components/ArticleDetail.vue
+++ blogvue/src/components/ArticleDetail.vue
@@ -2,18 +2,18 @@
     <el-row v-loading="loading">
         <el-col :span="24">
             <div style="text-align: left;">
-                <el-button type="text" icon="el-icon-back" @click="goBack" style="padding-bottom: 0px;">返回</el-button>
+                <el-button type="text" icon="el-icon-back" @click="goBack" style="padding-bottom: 0;">返回</el-button>
             </div>
         </el-col>
         <el-col :span="24">
             <div>
-                <div><h3 style="margin-top: 0px;margin-bottom: 0px">{{article.title}}</h3></div>
+                <div><h3 style="margin-top: 0;margin-bottom: 0">{{article.title}}</h3></div>
                 <div style="width: 100%;margin-top: 5px;display: flex;justify-content: flex-end;align-items: center">
                     <div style="display: inline; color: #20a0ff;margin-left: 50px;margin-right:20px;font-size: 12px;">
                         用户 {{article.userName}}
                     </div>
                     <span style="color: #20a0ff;margin-right:20px;font-size: 12px;">话题 {{article.topicName }}</span>
-                    <span style="margin:0px 50px 0px 0px"></span>
+                    <span style="margin:0 50px 0 0"/>
                 </div>
             </div>
         </el-col>
@@ -35,7 +35,7 @@
         },
         mounted: function () {
             let aid = this.$route.query.aid;
-            window.console.log(this.$route.query.aid);
+            // window.console.log(this.$route.query.aid);
             let _this = this;
             this.loading = true;
             getRequest("/article/id/" + aid).then(resp => {
